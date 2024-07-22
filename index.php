e3f48798d18a5ff4bef06351495455ffbd7aab3e
Lailyn Puad <lailynfuad@gmail.com>
2024-07-22 16:51:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>UAS Rekayasa Web</title>
+	<!-- This file has been cloned from https://github.com/lailyn/uas-rw-2024 -->
+</head>
+<body>
+
+</body>
+</html>
